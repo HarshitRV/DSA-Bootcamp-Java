a8a0b7af7c5346a2704e9d8bba0cc70415345267
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-09-27 15:26:10 +0530

    changes to readme and lecture/05

--- lectures/05-first-java-program/first-idea-program/src/com/kunal/Inputs.java
+++ lectures/05-first-java-program/first-idea-program/src/com/kunal/Inputs.java
@@ -4,15 +4,23 @@ import java.util.Scanner;
 
 public class Inputs {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-//        System.out.print("Please enter some input: ");
-//        int rollno = input.nextInt();
-//        System.out.println("Your roll number is " + rollno);
+       Scanner input = new Scanner(System.in);
+       System.out.print("Please enter a number : ");
+       int rollno = input.nextInt();
+       System.out.println("Your number is " + rollno);
+    
+       System.out.print("Please enter a string : ");
+       String name = input.next();
+       System.out.println(name);
 
-//        String name = input.next();
-//        System.out.println(name);
+       System.out.print("Please enter a string sentence : ");
+        
+       input.nextLine();
+       String sentence = input.nextLine();
+       System.out.println(sentence);
 
-//        float marks = input.nextFloat();
-//        System.out.println(marks);
+       System.out.print("Please enter a float : ");
+       float marks = input.nextFloat();
+       System.out.println(marks);
     }
 }
